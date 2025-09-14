fd32c15f88c1d3b84234ed0d35560c0e0dcc6261
Jed Pham <haopv7@gmail.com>
2025-09-14 19:47:42 +0700

    update product sections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import HeroSection from "../../components/HeroSection/HeroSection";
 import IPhoneAirSection from "../../components/ProductsSection/IPhoneAirSection/IPhoneAirSection";
 import IPhone17Section from "../../components/ProductsSection/iPhone17Section/iPhone17Section";
+import ProductsGridSection from "../../layouts/ProductsGridLayout";
 import styles from "./Home.module.scss";
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
       <HeroSection />
       <IPhoneAirSection />
       <IPhone17Section />
-
+      <ProductsGridSection />
       <section style={{ padding: "2rem", textAlign: "center" }}>
         <h2>Welcome to Apple Clone</h2>
         <p>Explore our latest products and innovations.</p>
